8dd45122cc6e590d9feb85f15306c481ebac3301
Marcin <marcin@marcinbazylak.com>
2020-06-22 14:37:35 +0200

    initial

---
+++ js/links.js
@@ -0,0 +1,26 @@
+$('#aboutMeLink').click(function() {
+   // $('html, body').animate({
+   //      scrollTop: $('#aboutMe').offset().top
+   //  }, 500);
+   toggleMenu();
+   $('#input-toggle').prop('checked', false);
+
+});
+
+$('#portfolioLink').click(function() {
+   // $('html, body').animate({
+   //      scrollTop: $('#portfolio').offset().top
+   //  }, 500);
+   toggleMenu();
+   $('#input-toggle').prop('checked', false);
+
+});
+
+$('#contactLink').click(function() {
+   // $('html, body').animate({
+   //      scrollTop: $('#contact').offset().top
+   //  }, 500);
+   toggleMenu();
+   $('#input-toggle').prop('checked', false);
+
+});
---
+++ js/loading.js
@@ -0,0 +1,47 @@
+var s = 0.2;
+
+var y = $(document).scrollTop();
+var h = $(window).innerHeight();
+var cl = 4;
+
+$('.box3').each(function() {
+    var t = $(this).offset().top;
+    if (y + h - 50 > t) {
+
+      if(cl == 2){
+         cl = 4;
+      } else {
+         cl = 2;
+      }
+
+      $(this).css('animation-delay', s + 's');
+      $(this).addClass('box' + cl);
+      s += 0.5;
+
+    }
+  });
+
+$(document).scroll(function() {
+
+  var y = $(this).scrollTop();  
+  var h = $(window).height();
+
+  $('.box3').each(function() {
+    var t = $(this).offset().top;
+
+    if (y + h - 50 > t) {
+
+      if(cl == 2){
+         cl = 4;
+      } else {
+         cl = 2;
+      }
+
+      if(!$(this).hasClass('box2') && !$(this).hasClass('box4'))
+
+         $(this).addClass('box' + cl);
+         
+      }
+
+  });
+});
